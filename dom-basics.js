59369738c28bdba303fa1024975fc392d9257913
BeatrizPaola <97312758+BeatrizPaola@users.noreply.github.com>
2022-03-08 11:23:07 -0300

    Add files via upload

---
+++ dom-basics.js
@@ -0,0 +1,13 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+const newImage=document.createElement("img");
+newImage.setAttribute('src', 'https://placeimg.com/200/200/animals');
+newImage.setAttribute('alt', 'animales');
+document.body.appendChild(newImage);
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+const newSection =document.createElement("section");
+newSection.innerHTML="<h2>CSE121b</h2><p>Welcome to JavaScript Language</p>";
+document.body.appendChild(newSection);
