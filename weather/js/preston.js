46e3380b10993127c8c9b728d53f1abe447a4f4b
BeatrizPaola <tiz.sandoval@gmail.com>
2022-03-09 22:50:16 -0300

    Updated css file

--- weather/js/preston.js
+++ weather/js/preston.js
@@ -40,3 +40,15 @@ const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 
 document.getElementById("currentdate").textContent = fulldate;
+
+if (dayName == "Friday") {
+	var banner = document.createElement("div");
+	banner.className = "b";
+	banner.innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontSize = "x-large";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.textAlign = "center";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.padding = "20px 20px 20px 20px";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.backgroundColor = "#f5cf87";
+}
+
