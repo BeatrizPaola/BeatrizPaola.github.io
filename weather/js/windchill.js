8d6c928fea7a5cce94f218187849a4bd32a6de4e
BeatrizPaola <tiz.sandoval@gmail.com>
2022-03-11 15:35:40 -0300

    Updated wind chill

--- weather/js/windchill.js
+++ weather/js/windchill.js
@@ -3,4 +3,4 @@ let s = 11;
 let wc = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
 
 wc = Math.floor(wc)
-document.querySelector("#wind-chill").textContent = wc + '°F';
+document.getElementById("windchill").textContent = wc + '°F';
