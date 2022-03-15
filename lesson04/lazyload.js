e60869b2911f0302bae75d40a0d8811d9b23a0c0
BeatrizPaola <tiz.sandoval@gmail.com>
2022-03-15 16:47:20 -0300

    lesson 4 exersice lasy load images

---
+++ lesson04/lazyload.js
@@ -0,0 +1,25 @@
+const images = document.queryselectorAll("[data-src]");
+
+function preloadImage(img){
+    const src= img.getAtriutte("data-src");
+    if (!src){
+        return;
+    }
+    img.src=src;
+}
+const imgOptions={};
+
+const imgObserver =new IntersectionObserver((entries, imgObserver) =>{
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+        preloadImage(entry.target); 
+        imgObserver.unobserve(entry.target);   
+        }
+    })
+}, imgOptions);
+
+images.forEach (image =>{
+    imgObserver.observe(image);
+})
