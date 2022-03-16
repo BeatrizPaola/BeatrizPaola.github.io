b70d3721b129b427fd044e001d682b8f5c5e507a
BeatrizPaola <tiz.sandoval@gmail.com>
2022-03-16 14:45:39 -0300

    loadng images updated

--- lesson04/lazyload.js
+++ lesson04/lazyload.js
@@ -7,7 +7,10 @@ function preloadImage(img){
     }
     img.src=src;
 }
-const imgOptions={};
+const imgOptions={
+    threshold:1.0,
+    rootMargin: "0px 0px 300px 0px"
+};
 
 const imgObserver =new IntersectionObserver((entries, imgObserver) =>{
     entries.forEach(entry =>{
